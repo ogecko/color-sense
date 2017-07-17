2f457756d69f5ef68090133436789894ecce3b86
David Morrison <jdmorriso@gmail.com>
2017-07-17 16:59:15 +1000

    enh(sidebar): change sidebar to more tailored ui for thresholds and masks

--- client/views/panelForSettings.js
+++ client/views/panelForSettings.js
@@ -2,41 +2,67 @@ import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import { store } from '/imports/store/index.js';
 
-
 Template.panelForSettings.onCreated(function() {
 	const self = this;
 	store.subscribe(self);
 });
 
-function onFormChangesSetStore(self, formSelector, storeId) {
-	// NOTE to have a Form "get values" and "set values" working compitibly
-	// need FIX to lines 561 and 579 semantic-ui/definitions/behaviours/form.js
-	// to get values[name].push(true) rather then 'on'.
-
-	// Initialise form with any values from the store (and when they change)
-	self.autorun(() => {
-		const doc = store.get(storeId);
-		if (doc && doc.isReady) {
-			self.$(formSelector).form('set values', _.omit(doc, ['_id', 'isReady']));
-		}
-	});
-
-	// Update the store whenever any field is changed
-	self.$(`${formSelector} .ui.checkbox`).checkbox({
-		onChange: () => store.set(storeId, self.$(formSelector).form('get values')),
-	});
-	self.$(`${formSelector} .ui.dropdown`).dropdown({
-		onChange: () => store.set(storeId, self.$(formSelector).form('get values')),
-	});
-
-}
-
 Template.panelForSettings.onRendered(function () {
 	const self = this;
-
-	// Link each form to the store
-	onFormChangesSetStore(self, '.ui.threshold.form', 'thresholdSettings');
-	onFormChangesSetStore(self, '.ui.view.form', 'viewSettings');
+	self.$('.ui.checkbox').checkbox();
+	self.$('.ui.dropdown').dropdown();
 });
 
+Template.panelForSettings.helpers({
+	numLevels: () => { const n = store.get('thresholdSettings').numEdges / 10 + 1; return n == 1 ? 'All' : n},
+	isLevels: () => store.get('thresholdSettings').numEdges > 0,
+	levels: () => _.range(1, store.get('thresholdSettings').numEdges / 10 + 2),
+	isMasked: (lvl) => {
+		const settings = store.get('thresholdSettings');
+		if (lvl < settings.maskLevel / 10 + 1) return 'primary';
+		if (lvl > settings.numEdges / 10 - settings.maskLight / 10 + 1) return 'primary';
+		return undefined;
+	},
+	isShowColors: () => store.get('thresholdSettings').showColors ? 'checked' : undefined,
+	isShowSoftEdges: () => store.get('thresholdSettings').showSoftEdges ? 'checked' : undefined,
+});
+
+function settingsMutate(mutateFn) {
+		let settings = store.get('thresholdSettings');
+		settings = mutateFn(settings);
+		store.set('thresholdSettings', settings);
+}
+
+Template.panelForSettings.events({
+	'click .js-onLevelDec': () => settingsMutate(s => {
+		s.numEdges = (90 + s.numEdges - 10) % 90;
+		return s;
+	}),
+	'click .js-onLevelInc': () => settingsMutate(s => {
+		s.numEdges = (90 + s.numEdges + 10) % 90;
+		return s;
+	}),
+	'click .js-onLevelMid': () => settingsMutate(s => {
+		if (s.maskLight === 0 && s.maskLevel === 0) s.numEdges = 0;
+		s.maskLight = 0; s.maskLevel = 0;
+		return s;
+	}),
+	'click .js-onMask': (ev) => settingsMutate(s => {
+		const lvl = ev.currentTarget.dataset.level*10;
+		const darkDelta = lvl - s.maskLevel;
+		const lightDelta = +s.numEdges+20 - s.maskLight - lvl;
+		if (darkDelta === 0) {
+			s.maskLevel = s.maskLevel - 10;			// toggle current setting
+		} else if (lightDelta === 0) {
+			s.maskLight = s.maskLight - 10;			// toggle current setting
+		} else if (darkDelta < lightDelta) {
+			s.maskLevel = lvl;						// change dark mask
+		} else {
+			s.maskLight = +s.numEdges+20 - lvl;		// change light mask
+		}
+		return s;
+	}),
+	'click .js-showColors': () => settingsMutate(s => { s.showColors = !s.showColors; return s; }),
+	'click .js-showSoftEdges': () => settingsMutate(s => { s.showSoftEdges = !s.showSoftEdges; return s; }),
+});
 
