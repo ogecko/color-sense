7f4cb7804a36f651fb0550213559a44088e3cb37
David Morrison <jdmorriso@gmail.com>
2017-08-15 16:59:35 +1000

    enh(ciecam02): implement remaining algorithms for converting to/from ciecam02

--- client/shaders/ciecam02.fs
+++ client/shaders/ciecam02.fs
@@ -10,6 +10,27 @@ varying vec2 v_uv;
 
 vec3 D65 = vec3(95.047, 100.000, 108.883);
 
+// linear transform matrix for lms = xyz * MCAT02
+mat3 MCAT02 = mat3(0.7328,0.4296,-0.1624,         -0.7036,1.6975,0.0061,         0.0030,0.0136,0.9834);
+mat3 MCAT02INV = mat3(1.096124,-0.278869,0.182745, 0.454369,0.473533,0.072098,  -0.009628,-0.005698,1.015326);
+
+// linear transform matrix for rgb = xyz * MsRGB
+mat3 MsRGB = mat3(3.2406,-1.5372,-0.4986,         -0.9689,1.8758,0.0415,         0.0557,-0.2040,1.0570);
+mat3 MsRGBINV = mat3(0.4124,0.3576,0.1805,         0.2126,0.7152,0.0722,         0.0193,0.1192,0.9505);
+
+// linear tranform matrix for lms_p = lms_c * MHPE = lms_c * (MH * MCAT02INV) 
+mat3 MHPE = mat3(0.740979,0.218025,0.04101,        0.285353,0.624202,0.090445,  -0.0096276,-0.005698,1.015326);
+mat3 MHPEINV = mat3(1.55915,-0.544723,-0.014445,  -0.71433,1.85031,-0.13598,     0.010776,0.0052188,0.984006);
+
+// linear tranform matrix for p2ab = lms_p * MP2AB 
+mat3 MP2AB = mat3(2.0000,1.0000,0.05000,           1.0000,-1.0909,0.090909,     0.111111,0.111111,-0.222222);
+mat3 MP2ABINV = mat3(0.32787,0.32145,0.20527,      0.32787,-0.63507,-0.18603,   0.327869,-0.15681,-4.490378);
+
+
+///////////////////////// sRGB /////////////////////////////////////////
+// fwd XYZ [0..100] -> RGB [0..1] 
+// rev RGB [0..1] -> // XYZ [0..100] normalised to Y=100
+
 // compand a linear RGB component c into a nonlinear space
 float fwd_sRGB(float c) {
 	return (float(c > 0.0031308) * 1.055 * pow(c, (1.0 / 2.4)) - 0.055 
@@ -25,8 +46,7 @@ float rev_sRGB(float c) {
 // convert xyz to sRGB linear then compand to sRGB
 vec4 fwd_xyz2rgb(vec4 color) {
 	vec3 xyz = vec3(color.x, color.y, color.z) / 100.0;
-	mat3 M = mat3(3.2406, -1.5372, -0.4986,   -0.9689, 1.8758, 0.0415,  0.0557, -0.2040, 1.0570);
-	vec3 rgb = xyz * M;
+	vec3 rgb = xyz * MsRGB;
 	return vec4(fwd_sRGB(rgb.r), fwd_sRGB(rgb.g), fwd_sRGB(rgb.b), color.a);
 }
 
@@ -34,9 +54,12 @@ vec4 fwd_xyz2rgb(vec4 color) {
 vec4 rev_rgb2xyz(vec4 color) {
 	//Observer. = 2.0°, Illuminant = D65
 	vec3 rgb = vec3(rev_sRGB(color.r), rev_sRGB(color.g), rev_sRGB(color.b)) * 100.0;							
-	mat3 M = mat3(0.4124, 0.3576, 0.1805,    0.2126, 0.7152, 0.0722,    0.0193, 0.1192, 0.9505);
-	return vec4(rgb * M, color.a);
+	return vec4(rgb * MsRGBINV, color.a);
 }
+
+///////////////////////// Lab/lch /////////////////////////////////////////
+// fwd XYZ [0..100] -> L[0..100] AB[-128..+128] -> L[0..100] C[0..100] H[0..360]
+// rev L[0..100] C[0..100] H[0..360] -> L[0..100] AB[-128..+128] -> XYZ [0..100]
 
 // convert xyz component to lab component
 float fwd_lab(float c) {
@@ -85,18 +108,136 @@ vec4 rev_lch2lab(vec4 color) {
 }
 
 // convert rgb to lch 
-vec4 rgb_to_lch(vec4 color) {
+vec4 fwd_rgb2lch(vec4 color) {
 	vec4 xyz = rev_rgb2xyz(color);
 	vec4 lab = fwd_xyz2lab(xyz);
 	vec4 lch = fwd_lab2lch(lab);
 	return lch;
 }
 
-
 // convert lch to rgb 
-vec4 lch_to_rgb(vec4 color) {
+vec4 rev_lch2rgb(vec4 color) {
 	vec4 lab = rev_lch2lab(color);
 	vec4 xyz = rev_lab2xyz(lab);
+	vec4 rgb = fwd_xyz2rgb(xyz);
+	return rgb;
+}
+
+///////////////////////// CIECAM02 /////////////////////////////////////////
+// fwd XYZ [0..100] -> J[0..100] C[0..100] H[0..360]
+// rev J[0..100] C[0..100] H[0..360] -> XYZ [0..100]
+// 
+// convert lms_p component to lms_a component
+float fwd_jch(float c, float FL) {
+   float x = pow(FL * abs(c) / 100.0, 0.42);
+   return sign(c) * 400.0 * x / (27.13 + x) + 0.1;
+}
+
+// convert lms_a component to lms_p component
+float rev_jch(float c, float FL) {
+   float x = c - 0.1 ;
+   return sign(x) * 100.0 / FL * pow(27.13 * abs(x) / (400.0 - abs(x)), 1.0/0.42);
+}
+
+// calculate surround parameters F (degree of adaption factor), c (impact of surround), Nc (chromatic induction factor)
+// all based on S (surround factor [0=dark .. 1=dim .. 2=average])
+vec3 calc_FcN(float S) {
+	float F = float(S < 1.0)*(0.8 + 0.1*S)     + float(S >= 1.0)*(0.9 + 0.1*(S - 1.0));
+	float c = float(S < 1.0)*(0.525 + 0.065*S) + float(S >= 1.0)*(0.59 + 0.1*(S - 1.0));
+	float Nc = float(S < 1.0)*(0.8 + 0.15*S)   + float(S >= 1.0)*(0.95 + 0.05*(S - 1.0));
+	return vec3(F, c, Nc);
+}
+
+// calculate D degree of adaption components and F_L luminance adaption based on viewing conditions
+// xyz_w (adopting white point), La (luminance of adapting field) and F (surround 1.0=average, 0.9=dim, 0.8=dark)
+vec4 calc_DF(vec3 xyz_w, float La, vec3 FcN) {
+	vec3 lms_w = xyz_w * MCAT02;
+	float D = FcN.x * (1.0 - 1.0 / 3.6 * exp(-(La + 42.0) / 92.0));
+	float K = 1.0 / (5.0 * La + 1.0);
+	return vec4(
+		xyz_w.y / lms_w.x * D + 1.0 - D,  	// D_L
+		xyz_w.y / lms_w.y * D + 1.0 - D,  	// D_M
+		xyz_w.y / lms_w.z * D + 1.0 - D, 	// D_S
+		(0.2 * pow(K, 4.0) * 5.0 * La + 0.1 * pow(1.0 - pow(K, 4.0), 2.0) * pow(5.0 * La, 1.0/3.0))		// F_L
+	);
+}
+
+// calculate A_w, Nbb, z and n factors
+// xyz_w (adopting white point), La (luminance of adapting field), Yb (background) amd DF prev calcs
+vec4 calc_ANZn(vec3 xyz_w, float La, float Yb, vec4 DF) {
+	vec3 lms_wp = ((xyz_w * MCAT02) * vec3(DF)) * MHPE;
+	vec3 lms_wa = vec3(fwd_jch(lms_wp.x, DF.w), fwd_jch(lms_wp.y, DF.w), fwd_jch(lms_wp.z, DF.w));
+	float n = Yb / xyz_w.y;
+	float Nbb = 0.725 * pow(1.0 / n, 0.2);
+	float z = 1.48 + sqrt(n);
+	float Aw = (lms_wa.x*2.0 + lms_wa.y + lms_wa.z*0.05 - 0.305) * Nbb;
+	return vec4(Aw, Nbb, z, n);
+}
+
+// convert xyz to lab
+vec4 fwd_xyz2jch(vec4 color) {
+	vec3 FcN = calc_FcN(1.0);
+    vec4 DF = calc_DF(D65, 100.0, FcN); 
+    vec4 ANZn = calc_ANZn(D65, 100.0, 20.0, DF); 
+
+	vec3 xyz = vec3(color);
+	vec3 lms_p = ((xyz * MCAT02) * vec3(DF)) * MHPE;
+	vec3 lms_a = vec3(fwd_jch(lms_p.x, DF.w), fwd_jch(lms_p.y, DF.w), fwd_jch(lms_p.z, DF.w));
+	vec3 Aab = lms_a * MP2AB; Aab.x = (Aab.x - 0.305) * ANZn.y;
+	float J = 100.0 * pow(Aab.x / ANZn.x, FcN.y * ANZn.z); 
+	float h_rad = atan(Aab.z, Aab.y); 			//in GLSL this is atan2
+	float h = degrees(h_rad) + float(degrees(h_rad) < 0.0) * 360.0;
+	float e_t = 1.0/4.0 * (cos(h_rad + 2.0) + 3.8);
+	float t = (50000.0 / 13.0 * FcN.z * ANZn.w * e_t * sqrt(Aab.y*Aab.y + Aab.z*Aab.z) 
+			  / (lms_a.x + lms_a.y + 21.0 / 20.0 * lms_a.z));
+	float C = pow(t, 0.9) * sqrt(J / 100.0) * pow(1.64 - pow(0.29, ANZn.w), 0.73);
+	return vec4(J, C, h, color.a);
+}
+ 
+// convert lab to xyz
+vec4 rev_jch2xyz(vec4 color) {
+	vec3 FcN = calc_FcN(1.0);
+    vec4 DF = calc_DF(D65, 100.0, FcN); 
+    vec4 ANZn = calc_ANZn(D65, 100.0, 20.0, DF); 
+
+    float J = color.x; float C = color.y; float h_rad = radians(color.z);
+    float A = ANZn.x * pow(J / 100.0, 1.0 / FcN.y / ANZn.z);
+	float t = pow(C / (sqrt(J / 100.0) * pow(1.64 - pow(0.29, ANZn.w), 0.73)), 10.0 / 9.0);
+	float e_t = 1.0 / 4.0 * (cos(h_rad + 2.0) + 3.8);   
+	float p_1 = 50000.0 / 13.0 * FcN.z * ANZn.w * e_t / t;
+	float p_2 = A / ANZn.y + 0.305;
+	float q_1 = p_2 * 61.0/20.0 * 460.0/1403.0;
+	float q_2 = 61.0/20.0 * 220.0/1403.0;
+	float q_3 = 21.0/20.0 * 6300.0/1403.0 - 27.0/1403.0;
+	float sin_h = sin(h_rad);
+	float cos_h = cos(h_rad);
+	vec3 Aab = vec3(A / ANZn.y + 0.305, 0.0, 0.0);
+	if (t != 0.0) {
+		if (abs(sin_h) >= abs(cos_h)) {
+			Aab.z = q_1 / (p_1 / sin_h + q_2 * cos_h / sin_h + q_3); 
+			Aab.y = Aab.z * cos_h / sin_h; 
+		} else {
+			Aab.y = q_1 / (p_1 / cos_h + q_2 + q_3 * sin_h / cos_h); 
+			Aab.z = Aab.y * sin_h / cos_h; 
+		}
+	}
+	vec3 lms_a = Aab * MP2ABINV;
+	vec3 lms_p = vec3(rev_jch(lms_a.x, DF.w), rev_jch(lms_a.y, DF.w), rev_jch(lms_a.z, DF.w));
+	vec3 xyz = ((lms_p * MHPEINV) * MCAT02INV) / vec3(DF);
+	return vec4(xyz, color.a);
+}
+
+
+// convert rgb to ciecam02 jch
+vec4 fwd_rgb2jch(vec4 color) {
+	vec4 xyz = rev_rgb2xyz(color);
+	vec4 jch = fwd_xyz2jch(xyz);
+	return jch;
+}
+
+// convert ciecam02 to rgb 
+vec4 rev_jch2rgb(vec4 color) {
+	vec4 xyz = rev_jch2xyz(color);
 	vec4 rgb = fwd_xyz2rgb(xyz);
 	return rgb;
 }
@@ -108,7 +249,8 @@ float hatch() {
 }
 
 void main( void ) {
-	vec4 lch = rgb_to_lch(texture2D(u_tex0, v_uv));
+	vec4 lch = fwd_rgb2jch(texture2D(u_tex0, v_uv));
+
 	if (u_numEdges > 9.0) {					// u_numEdges 0 = no edges, 10 = 1 edge, 20 = 2 edges, 30 = 3 edges, etc.
 		float dx = 1000.0 / (u_numEdges);		// dx is width of grey bands 100 for 1 edge, 50 for 2 edges, 33 for 3 edges, 25 for 4 edges, etc
 		float lx = max(lch.x, dx * u_maskDark / 10.0);				// clamp out n dark bands eg 0 = show all, 10 = no blacks, 20=no blacks or darks
@@ -128,5 +270,6 @@ void main( void ) {
 
 	lch.y = lch.y * (1.0 + u_maxContrast/50.0);														// optionally amp the chroma
 	lch.y = lch.y * u_showColors / 100.0;
-	gl_FragColor = lch_to_rgb(lch);
+
+	gl_FragColor = rev_jch2rgb(lch);
 }
