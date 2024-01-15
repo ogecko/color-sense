a56fa3276fd75bc50e209b568f7ffdd5b5b4d7fb
David Morrison <jdmorriso@gmail.com>
2024-01-15 15:38:40 +1100

    enh(outline): add canny edge detection algorithm

--- client/shaders/ciecam02.fs
+++ client/shaders/ciecam02.fs
@@ -10,6 +10,7 @@ uniform float u_qChroma;
 uniform float u_showEdges;
 uniform float u_maxContrast;
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform sampler2D u_tex0;
 uniform int u_numLevels;
 uniform float u_nodes[100];		// list of node data for value levelling, array of values, each from 0 to 100.
@@ -339,16 +340,114 @@ vec4 blur_rgb2jch() {
 	return jch;
 }
 
+// #define thickness 3.
+
+float getAve(vec2 uv){
+    vec3 rgb = texture2D(u_tex0, uv).rgb;
+    vec3 lum = vec3(0.299, 0.587, 0.114);
+    return dot(lum, rgb);
+}
+
+// Detect edge.
+vec4 sobel(vec2 fragCoord, vec2 dir){
+    vec2 uv = fragCoord/u_resolution.xy;
+    vec2 texel = 1./u_resolution.xy;
+	float thickness = max(1., u_edgeRadius);
+    float np = getAve(uv + (vec2(-1,+1) + dir ) * texel * thickness);
+    float zp = getAve(uv + (vec2( 0,+1) + dir ) * texel * thickness);
+    float pp = getAve(uv + (vec2(+1,+1) + dir ) * texel * thickness);
+    
+    float nz = getAve(uv + (vec2(-1, 0) + dir ) * texel * thickness);
+    // zz = 0
+    float pz = getAve(uv + (vec2(+1, 0) + dir ) * texel * thickness);
+    
+    float nn = getAve(uv + (vec2(-1,-1) + dir ) * texel * thickness);
+    float zn = getAve(uv + (vec2( 0,-1) + dir ) * texel * thickness);
+    float pn = getAve(uv + (vec2(+1,-1) + dir ) * texel * thickness);
+    
+    // np zp pp
+    // nz zz pz
+    // nn zn pn
+    
+    // SOBEL filter - standard operator
+    // float gx = (np*-1. + nz*-2. + nn*-1. + pp*1. + pz*2. + pn*1.);
+    // float gy = (np*-1. + zp*-2. + pp*-1. + nn*1. + zn*2. + pn*1.);
+    // SOBEL filter - most common, better rotational symmetry, // https://www.shadertoy.com/view/Wds3Rl
+    // float gx = (np*-3. + nz*-10. + nn*-3. + pp*3. + pz*10. + pn*3.);
+    // float gy = (np*-3. + zp*-10. + pp*-3. + nn*3. + zn*10. + pn*3.);
+    // SOBEL filter - optimised 8 bit operator, derivative kernel, https://en.wikipedia.org/wiki/Sobel_operator#Alternative_operators 
+    float gx = (np*-47. + nz*-162. + nn*-47. + pp*47. + pz*162. + pn*47.);
+    float gy = (np*-47. + zp*-162. + pp*-47. + nn*47. + zn*162. + pn*47.);
+    
+    vec2 G = vec2(gx,gy);
+    
+    float grad = length(G);
+    
+    float angle = atan(G.y, G.x);
+    
+    return vec4(G, grad, angle);
+}
+// Make edge thinner.
+vec2 hysteresisThr(vec2 fragCoord, float mn, float mx){
+
+    vec4 edge = sobel(fragCoord, vec2(0));
+
+    vec2 dir = vec2(cos(edge.w), sin(edge.w));
+    dir *= vec2(-1,1); // rotate 90 degrees.
+    
+    vec4 edgep = sobel(fragCoord, dir);
+    vec4 edgen = sobel(fragCoord, -dir);
+	// supress this edge  - if this gradient is less than gradient in positive and negative direction
+    if(edge.z < edgep.z || edge.z < edgen.z ) edge.z = 0.;
+    // vec4 edgep2 = sobel(fragCoord, dir*2.);
+    // vec4 edgen2 = sobel(fragCoord, -dir*2.);
+    // if(edge.z < edgep.z || edge.z < edgen.z || edge.z < edgep2.z || edge.z < edgen2.z ) edge.z = 0.;
+    
+    return vec2(
+        (edge.z > mn) ? edge.z : 0.,		// weak edge (or strong edge)
+        (edge.z > mx) ? edge.z : 0.			// strong edge
+    );
+}
+
+float cannyEdge(vec2 fragCoord, float mn, float mx){
+
+    vec2 np = hysteresisThr(fragCoord + vec2(-1,+1), mn, mx);
+    vec2 zp = hysteresisThr(fragCoord + vec2( 0,+1), mn, mx);
+    vec2 pp = hysteresisThr(fragCoord + vec2(+1,+1), mn, mx);
+    
+    vec2 nz = hysteresisThr(fragCoord + vec2(-1, 0), mn, mx);
+    vec2 zz = hysteresisThr(fragCoord + vec2( 0, 0), mn, mx);
+    vec2 pz = hysteresisThr(fragCoord + vec2(+1, 0), mn, mx);
+    
+    vec2 nn = hysteresisThr(fragCoord + vec2(-1,-1), mn, mx);
+    vec2 zn = hysteresisThr(fragCoord + vec2( 0,-1), mn, mx);
+    vec2 pn = hysteresisThr(fragCoord + vec2(+1,-1), mn, mx);
+    
+    // np zp pp
+    // nz zz pz
+    // nn zn pn
+
+	// supress if below weak, or (strong and no neighbor strong)
+    return min(1., step(1e-2, zz.x*8.) * step(1e-2, (np.y + zp.y + pp.y + nz.y + pz.y + nn.y + zn.y + pn.y)*8.));
+}
+
 
 void main( void ) {
 	vec4 jch1 = fwd_rgb2jch(texture2D(u_tex0, v_uv), D65, 100.0, 20.0, 1.0);
 	vec4 jch2 = adjust_jch(jch1);
 
 	if (u_showEdges>0.0) {
-		// EDGE adjustment (a) optionally outline all major shifts in tone
-		vec4 jch3 = blur_rgb2jch();
+		// EDGE adjustment (a) optionally outline using gaussian difference
+		// vec4 jch3 = blur_rgb2jch();
+		// float t = u_showEdges / 100.0 * (100.0 - u_opacity) / 100.0;
+		// jch2.x = mix(jch2.x, 100.0-(step(u_edgeDb*2.0 + 2.0, jch3.x-jch2.x)*100.0), t);
+
+		// EDGE adjustment (b) optionally outline using canny edge detection
+		vec2 fragCoord = v_uv*u_resolution.xy;
+		float threshold = u_edgeDb*8. + 3.;
+		float edge = cannyEdge(fragCoord, threshold/2., threshold);  // The low threshold is typically set to 1/2 of the high threshold
 		float t = u_showEdges / 100.0 * (100.0 - u_opacity) / 100.0;
-		jch2.x = mix(jch2.x, 100.0-(step(u_edgeDb*2.0 + 2.0, jch3.x-jch2.x)*100.0), t);
+		jch2.x = mix(jch2.x, 100. - edge*100., t);
 
 	}
 
